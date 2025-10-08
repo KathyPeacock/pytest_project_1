320b4c35e878794ad99efe38bfcd611cbeec9f19
Kathy Peacock <kathypeacockcreative@gmail.com>
2025-10-08 18:13:06 +0100

    Integrated testing challenge (MusicTracker) completed

--- lib/music_tracker.py
+++ lib/music_tracker.py
@@ -10,20 +10,48 @@ class MusicTracker():
     def __init__(self):
         self.tracks = []
 
-    def add_track(self, name: str):
+# NB I had to change this lower case method for checking duplicates
+# so it deals with self.tracks which holds OBJECTS not strings.
 
-        tracks_lower = [tr.lower() for tr in self.tracks] 
-        if name.lower() not in tracks_lower:
-            self.tracks.append(name)
+    def add_track(self, track):
+        track_str = track.format().lower()
+        track_list_lower = [tr.format().lower() for tr in self.tracks]
+        if track_str not in track_list_lower:
+            self.tracks.append(track)
 
     def list_tracks(self):
-        return self.tracks
+        return [tr.format() for tr in self.tracks]
     
 
+
+# if __name__ == "__main__":
+#     my_tracker = MusicTracker()
+#     my_tracker.add_track("Poetry - Devin Kennedy")
+#     my_tracker.add_track("Papasito - Karol G")
+#     print(my_tracker.list_tracks())
+    
+
+# MusicTracker class stored strings with both artist and song name.
+# class Track will represent a song as an object
+# in this object attributes for each track will be Title and Artist.
+# MusicTracker will store a collection of Track objects.
+
+class Track():
+    def __init__(self, title, artist):
+        self.title = title
+        self.artist = artist
+
+# Make sure a nice string is returned with the track attribute info.
+
+    def format(self):
+        return f"{self.title} by {self.artist}"
 
 if __name__ == "__main__":
     my_tracker = MusicTracker()
-    my_tracker.add_track("Poetry - Devin Kennedy")
-    my_tracker.add_track("Papasito - Karol G")
+    track_1 = Track("Poetry", "Devin Kennedy")
+    track_2 = Track("Papasito", "Karol G")
+
+    my_tracker.add_track(track_1)
+    my_tracker.add_track(track_2)
+
     print(my_tracker.list_tracks())
-    
--- tests/test_music_tracker.py
+++ tests/test_music_tracker.py
@@ -13,26 +13,47 @@ def test_initially_empty_list():
 
 def test_add_single_track_creates_list():
     tracker = MusicTracker()
-    tracker.add_track("Rainbow High - Rachel Zegler")
-    assert tracker.list_tracks() == ["Rainbow High - Rachel Zegler"]
+    tracker.add_track(Track("Rainbow High", "Rachel Zegler"))
+    assert tracker.list_tracks() == ["Rainbow High by Rachel Zegler"]
+
+def test_add_and_list_multiple_tracks_preserves_order():
+    tracker = MusicTracker()
+    track_1 = Track("Poetry", "Devin Kennedy")
+    track_2 = Track("Papasito", "Karol G")
+    track_3 = Track("Nothing", "Bruno Major")
+
+    tracker.add_track(track_1)
+    tracker.add_track(track_2)
+    tracker.add_track(track_3)
+
+    assert tracker.list_tracks() == ["Poetry by Devin Kennedy", "Papasito by Karol G", "Nothing by Bruno Major"]
 
 
 # test multiple tracks added in order
 
-def test_add_multiple_tracks_preserves_order():
-    tracker = MusicTracker()
-    tracker.add_track("Rainbow High - Rachel Zegler")
-    tracker.add_track("Poetry - Devin Kennedy")
-    tracker.add_track("Papasito - Karol G")
-    assert tracker.list_tracks() == ["Rainbow High - Rachel Zegler", "Poetry - Devin Kennedy", "Papasito - Karol G"]
+# def test_add_multiple_tracks_preserves_order():
+#     tracker = MusicTracker()
+#     tracker.add_track("Rainbow High - Rachel Zegler")
+#     tracker.add_track("Poetry - Devin Kennedy")
+#     tracker.add_track("Papasito - Karol G")
+#     assert tracker.list_tracks() == ["Rainbow High - Rachel Zegler", "Poetry - Devin Kennedy", "Papasito - Karol G"]
 
 
 # doesn't add duplicate tracks (case insensitive)
 
-def test_does_not_add_duplicate_tracks_case_insensitive():
+def test_does_not_add_duplicate_tracks_case_different():
     tracker = MusicTracker()
-    tracker.add_track("Papasito - Karol G")
-    tracker.add_track("papasito - KAROL G")
-    assert tracker.list_tracks() == ["Papasito - Karol G"]
+    tracker.add_track(Track("Papasito", "Karol G"))
+    tracker.add_track(Track("papasito", "KAROL G"))
+    assert tracker.list_tracks() == ["Papasito by Karol G"]
+
+def test_does_not_add_duplicate_tracks_case_same():
+    tracker = MusicTracker()
+    tracker.add_track(Track("Papasito", "Karol G"))
+    tracker.add_track(Track("Papasito", "Karol G"))
+    assert tracker.list_tracks() == ["Papasito by Karol G"]
 
     
+def test_track_format_returns_title_by_artist():
+    track = Track("Nothing", "Bruno Major")
+    assert track.format() == "Nothing by Bruno Major"
